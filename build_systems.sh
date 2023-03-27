ea13d14e589239834b15b31e2ff0b624115b1c93
Kloppel <jesse-jones@live.de>
2023-03-27 17:48:37 +0200

    Built smaller systems f/ quicker simulations

---
+++ build_systems.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+charmm <step0_build_system_Pmoxox.inp >Pmoxox_new.out
+charmm <step0_build_system_Pmredox.inp >Pmredox_new.out
+charmm <step0_build_system_Proxred.inp >Proxred_new.out
+
+charmm <step0_build_system_Foxox.inp >Foxox_new.out
+charmm <step0_build_system_Foxred.inp >Foxred_new.out
+charmm <step0_build_system_Fredox.inp >Fredox_new.out
+
+charmm <step0_build_system_Ooxox.inp >Ooxox_new.out
+charmm <step0_build_system_Ooxred.inp >Ooxred_new.out
+charmm <step0_build_system_Oredox.inp >Oredox_new.out
+
+charmm <step0_build_system_Eoxox.inp >Eoxox_new.out
+charmm <step0_build_system_Eoxred.inp >Eoxred_new.out
+charmm <step0_build_system_Eredox.inp >Eredox_new.out
+
+charmm <step0_build_system_Roxox.inp >Roxox_new.out
